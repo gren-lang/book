3979e34902e02178a69d1aaf1b2dc95592ee6bb8
Justin Blake <justin@blaix.com>
2024-04-22 07:09:05 -0400

    npx astro add starlight

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight()]
+});
